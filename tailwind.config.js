296abfc4f50f58490188545b7a0d87510676a718
zidanabbas <zidan.abbas28@gmail.com>
2022-04-09 01:10:59 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#f09605',
+        secondary: '#E0E0E0',
+        third: "#bfd0e0",
+        dark: '#404042',
+      },
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
