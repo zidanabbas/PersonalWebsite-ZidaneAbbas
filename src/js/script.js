4fcedc395746250a02dfc4cd19348a36ac5bed4f
zidanabbas <zidan.abbas28@gmail.com>
2023-09-01 00:11:55 +0700

    menambahkan link cv

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,52 @@
 // Navbar Fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    const toTop = document.querySelector('#to-top');
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-        toTop.classList.remove('hidden');
-        toTop.classList.add('flex');
-    }else {
-        header.classList.remove('navbar-fixed');
-        toTop.classList.remove('flex');
-        toTop.classList.add('hidden');
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
 };
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // klik diluar hamburger
-window.addEventListener('click', function (e){
-    if (e.target != hamburger && e.target != navMenu) {
-        hamburger.classList.remove('hamburger-active');
-        navMenu.classList.add('hidden');
-    }
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
 
-// Dark Menu 
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+// Dark Menu
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', function() {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-    } else {
-        html.classList.remove('dark');
-    }
-})
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
+
+function unduhFile() {
+  var link = document.createElement("a");
+  link.href = "src/CV/zidane-abbas.pdf";
+  link.download = "zidane-abbas.pdf";
+  link.click();
+}
